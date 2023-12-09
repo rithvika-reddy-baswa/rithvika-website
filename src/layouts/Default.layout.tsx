616c6c1dbef8a91a119ae333ec37a8436ce413d7
Priyatham-sai-chand <priyathamsaichand@gmail.com>
2023-12-08 16:59:47 -0800

    readd navbar component

--- src/layouts/Default.layout.tsx
+++ src/layouts/Default.layout.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 import { NextSeo } from 'next-seo';
 
+import { Navbar } from '~/components';
 import { usePersistantState, useSeoProps } from '~/lib';
 
 import type { WithChildren, WithProps } from '~/types';
